2943ee1edf603ab172f22f8d96429b460012ce64
enthus1ast <enthus1ast>
2017-02-23 16:06:13 +0100

    Signed-off-by: enthus1ast <enthus1ast>

---
+++ lamport.nim
@@ -0,0 +1,36 @@
+## The Lamport timestamp/clock
+
+type 
+  Lamport = int
+ 
+proc newLamport(initialValue: int = 0): Lamport = 
+  result = initialValue
+
+proc send(lamport: var Lamport) = 
+  lamport.inc 1
+
+proc recv(lamport: var Lamport, remoteTimestampt: int = 0)=
+  lamport = max(lamport, remoteTimestampt) + 1
+
+
+when isMainModule:
+  var timestamp = newLamport()
+  var remoteTimestamp = newLamport(23) # remote runs for a time
+  timestamp.recv()
+  remoteTimestamp.recv()
+
+  timestamp.recv()
+  remoteTimestamp.recv()
+
+  timestamp.recv(remoteTimestamp)
+  remoteTimestamp.send()
+
+  timestamp.recv(5)
+  timestamp.recv(5)
+  timestamp.recv()
+  timestamp.recv()
+  timestamp.recv()
+  assert timestamp == 31
+
+  remoteTimestamp.recv(timestamp)
+  assert remoteTimestamp == 32
---
+++ lamport.nimble
@@ -0,0 +1,11 @@
+# Package
+
+version       = "0.1.0"
+author        = "enthus1ast"
+description   = "The Lamport timestamp/clock"
+license       = "MIT"
+
+# Dependencies
+
+requires "nim >= 0.13.0"
+
